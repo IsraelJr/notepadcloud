ae20f58a5377177bf11245f3ea58a474135cc2cf
logonrm <logonrm@fiap.com.br>
2017-10-28 12:15:08 -0200

    Initial commit

---
+++ src/main/java/br/com/iasj/notepadcloud/componente/NotaComponent.java
@@ -0,0 +1,41 @@
+package br.com.iasj.notepadcloud.componente;
+
+import br.com.iasj.notepadcloud.model.Nota;
+import br.com.iasj.notepadcloud.repository.NotaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class NotaComponent {
+
+    @Autowired
+    NotaRepository notaRepository;
+
+    public Nota salvar(Nota nota){
+    List<Nota> notas = notaRepository.findByTitulo(nota.getTitulo());
+    if(notas.size() > 0) {
+        nota.setId(notas.get(0).getId());
+    }
+    return notaRepository.save(nota);
+    }
+
+    public List<Nota> findAll(){
+        return notaRepository.findAll();
+    }
+
+    public void deleteAll(){
+        notaRepository.deleteAll();
+    }
+
+    public Nota buscatNota(String titulo){
+        Nota nota;
+        List<Nota> notas = notaRepository.findByTitulo(titulo);
+        if(notas.isEmpty()){
+            return new Nota();
+        }else{
+            return notas.get(0);
+        }
+    }
+}
